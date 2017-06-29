95b8b088bd399ef4aa48ea0ba392a423e60f0000
fred <fred@yonzin.com>
2017-06-29 23:33:52 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+ #!/usr/bin/env bash
+REPO_DIR="~/repositories"
+
+sudo apt install vim zsh
+
+if [ -d $REPO_DIR]; then 
+  mkdir $REPO_DIR
+fi
+
+cd $REPO_DIR
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+wget -O ~/.oh-my-zsh/themes/docker.zsh-theme https://github.com/dpdornseifer/docker-zsh-theme/blob/master/docker.zsh-theme
+
+ 
