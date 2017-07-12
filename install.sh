8a764106d985bd28ce1529c83342c284ede27217
fred <fred@yonzin.com>
2017-07-12 13:22:36 +0200

    add docker in install script

--- install.sh
+++ install.sh
@@ -9,8 +9,36 @@ fi
 
 cd $REPO_DIR
 
+## INSTALL ZSH
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 wget -O ~/.oh-my-zsh/themes/docker.zsh-theme https://raw.githubusercontent.com/dpdornseifer/docker-zsh-theme/master/docker.zsh-theme
 
- 
+
+## INSTALL DOCKER
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual 
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update && sudo apt-get install docker-ce
+
+
+## POST INSTALL DOCKER
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker
+
+## INSTALL DOCKER-COMPOSE
+sudo curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
